2c60b47fdecec5d25a78f9dba5530d18b156a49b
NAHVUSdev <trentonenter@outlook.com>
2019-11-26 13:38:16 -0600

    modified index.js and added reviews sections

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ app.get('/', function (req, res) {
     res.sendFile('index.html', {root: path.join(__dirname, 'app')});
 });
 
+app.get('/reviews', function (req, res) {
+    "use strict";
+    res.sendFile('index.html', {root: path.join(__dirname, 'app/reviews')});
+});
+
+app.get('/reviews/:name', function (req, res, next) {
+  var options = {
+    root: path.join(__dirname, 'app/reviews'),
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }
+
+  var fileName = req.params.name
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
+  })
+});
+
 app.listen(8080);
